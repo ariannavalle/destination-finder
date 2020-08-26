97a8276d929b6827e1a6b69854ce2e92a6ee4533
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-08-26 19:46:51 -0400

    starter code template

---
+++ configs/db.config.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/destination-finder', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(x => console.log(`Successfully connected to the database ${x.connections[0].name}`))
+  .catch(error => {
+    console.error(`An error ocurred trying to connect to the database: `, error);
+    process.exit(1);
+  });
