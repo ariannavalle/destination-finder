a2528ab624e59c0fed1dce9c89d66fd73c26d4fd
Arianna Valle <61655861+ariannavalle@users.noreply.github.com>
2020-08-27 22:44:54 -0400

    Create filter model

--- models/filter.model.js
+++ models/filter.model.js
@@ -2,7 +2,23 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const filterSchema = new Schema(
-
+  {
+    continent: {
+      type: String
+    },
+    citySize: {
+      type: Number
+    },
+    budget: {
+      type: Number
+    },
+    categories: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
 );
 
 const Filter = model('Filter', filterSchema);
