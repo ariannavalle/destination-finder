7ef18d739e1362e0948e3e47df8012a8a57f741f
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-08-31 22:34:11 -0400

    add image search

--- routes/filter.routes.js
+++ routes/filter.routes.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const ddg = require("duckduckgo-images-api");
 const opentripAPIKey = process.env.OPENTRIPMAP_API_KEY;
 const openCageAPIKey = process.env.OPENCAGEDATA_API_KEY;
 
@@ -10,8 +11,21 @@ router.get('/', (req, res) => res.render('index'));
 
 router.post('/find-destination', (req, res, next) => {
 
-  function getAvgRating(citiesArray) {
-    return Object.values(citiesArray.reduce(function (obj, elem) {
+  async function searchImage(searchQuery){
+    const results = await ddg.image_search({
+      query: searchQuery,
+      iterations: 1,
+      moderate: true
+    })
+console.log('20', results[0].image)
+      if (results.length > 0) {
+        return results[0].image;
+      }
+      return ''
+  }
+
+  async function getCityInfo(citiesArray) {
+    const result = await citiesArray.reduce(async (obj, elem) => {
       if (!elem.city) return obj
       if (elem.city in obj) {
         obj[elem.city].count++;
@@ -19,27 +33,29 @@ router.post('/find-destination', (req, res, next) => {
         obj[elem.city].rate = Math.round(obj[elem.city].total_rating / obj[elem.city].count)
       }
       else {
+        const pic = await searchImage(elem.city);
+        console.log('37', pic)
         obj[elem.city] = {
           name: elem.city,
           count: 1,
           total_rating: elem.rating,
-          rate: elem.rating
+          rate: elem.rating,
+          img: pic
         }
       }
       return obj;
-    }, {}))
-    // return Object.keys(cityAndRatings).map(e => {return {[e]: cityAndRatings[e].total_rating/cityAndRatings[e].count}});
+    }, {});
+    console.log('48', result);
+    return Object.values(result);               
   }
 
   //calling this api returns different locations based on the coordinates and categories provided
   axios
     //@todo remove "&limit=100" before deploying
-    .get(`https://api.opentripmap.com/0.1/en/places/bbox?${req.body.coordinates}&kinds=${req.body.categories}&apikey=${opentripAPIKey}&limit=100`)
+    .get(`https://api.opentripmap.com/0.1/en/places/bbox?${req.body.coordinates}&kinds=${req.body.categories}&apikey=${opentripAPIKey}&limit=1`)
     .then(response => {
-      // console.log(response.data.features[0].geometry.coordinates);
-      // console.log(response.data.features[0].properties.rate);
-      // console.log(response.data.features[0].properties.name);
-
+      console.log(response.data.features[0].geometry.coordinates);
+      // console.log(response)
       const placeObj = [];
       const cities = [];
       response.data.features.forEach((place) => {
@@ -48,8 +64,8 @@ router.post('/find-destination', (req, res, next) => {
           .get(`https://api.opencagedata.com/geocode/v1/json?q=${place.geometry.coordinates[1]},${place.geometry.coordinates[0]}&key=${openCageAPIKey}&pretty=1&limit=1`);
         cities.push(prom);
       });
-
-      Promise.all(cities).then(citiesAPIResponse => {
+      // console.log(cities)
+      Promise.all(cities).then( async citiesAPIResponse => {
         citiesAPIResponse.forEach((data, i) => {
           // console.log(i, data.data.results[0].components.city)
 
@@ -61,10 +77,10 @@ router.post('/find-destination', (req, res, next) => {
             city: data.data.results[0].components.city
           });
         });
-        // console.log(placeObj)
-        // console.log(getAvgRating(placeObj));
 
-        res.render('destinations/destination-list', { places: getAvgRating(placeObj) });
+        const places = await getCityInfo(placeObj)
+        console.log('82',places);
+        res.render('destinations/destination-list', { places });
       })
         .catch(err => console.log(`Error finding the location's city: ${err}`));
 
