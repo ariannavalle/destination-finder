084f4cb7a0c4a813aba2f92ce6251e63fdd777a3
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-09-04 09:42:14 -0400

    Refactor filter route to use nearsphere

--- routes/filter.old.routes.js
+++ routes/filter.old.routes.js
@@ -2,31 +2,26 @@ require('dotenv').config();
 
 const express = require('express');
 const router = express.Router();
-
 const axios = require('axios');
 const ddg = require("duckduckgo-images-api");
-
-const City = require('../models/city.model');
-
-//API Keys
 const opentripAPIKey = process.env.OPENTRIPMAP_API_KEY;
 const openCageAPIKey = process.env.OPENCAGEDATA_API_KEY;
-
 
 router.get('/', (req, res) => res.render('index'));
 
 router.post('/find-destination', (req, res, next) => {
 
-  async function searchImage(searchQuery) {
+  async function searchImage(searchQuery){
     const results = await ddg.image_search({
       query: searchQuery,
       iterations: 1,
       moderate: true
     })
-    if (results.length > 0) {
-      return results[0].image;
-    }
-    return ''
+console.log('20', results[0].image)
+      if (results.length > 0) {
+        return results[0].image;
+      }
+      return ''
   }
 
   async function getCityInfo(citiesArray) {
@@ -39,6 +34,7 @@ router.post('/find-destination', (req, res, next) => {
       }
       else {
         const pic = await searchImage(elem.city);
+        console.log('37', pic)
         obj[elem.city] = {
           name: elem.city,
           count: 1,
@@ -49,58 +45,41 @@ router.post('/find-destination', (req, res, next) => {
       }
       return obj;
     }, {});
-    return Object.values(result);
+    console.log('48', result);
+    return Object.values(result);               
   }
 
   //calling this api returns different locations based on the coordinates and categories provided
   axios
-    //@todo remove "&limit=" before deploying
-    .get(`https://api.opentripmap.com/0.1/en/places/bbox?${req.body.coordinates}&kinds=${req.body.categories}&apikey=${opentripAPIKey}&limit=3`)
+    //@todo remove "&limit=100" before deploying
+    .get(`https://api.opentripmap.com/0.1/en/places/bbox?${req.body.coordinates}&kinds=${req.body.categories}&apikey=${opentripAPIKey}&limit=1`)
     .then(response => {
-      console.log("coordinates from opentripmap", response.data.features[0].geometry.coordinates);
+      console.log(response.data.features[0].geometry.coordinates);
+      // console.log(response)
       const placeObj = [];
       const cities = [];
-
       response.data.features.forEach((place) => {
-        const prom = 
-        City.find(
-          {
-            loc: {
-              $near: {
-                $geometry: {
-                  type: "Point",
-                  // coordinates : [ <longitude>, <latitude> ]    
-                  coordinates: [place.geometry.coordinates[0], place.geometry.coordinates[1]]
-                },
-                // if we set a max distance, we might not get any results
-                // $maxDistance: 900000
-              }
-            }
-          }
-        )
-            cities.push(prom);
+        //get coordinates of the locations returned and aggregates them by city
+        const prom = axios
+          .get(`https://api.opencagedata.com/geocode/v1/json?q=${place.geometry.coordinates[1]},${place.geometry.coordinates[0]}&key=${openCageAPIKey}&pretty=1&limit=1`);
+        cities.push(prom);
       });
-
       // console.log(cities)
-      Promise.all(cities).then(async citiesAPIResponse => {
+      Promise.all(cities).then( async citiesAPIResponse => {
         citiesAPIResponse.forEach((data, i) => {
           // console.log(i, data.data.results[0].components.city)
 
           placeObj.push({
-            coordinates: response.data.features[i].geometry.coordinates,
+            coordinates: response.data.features[i].geometry.coordinates, 
             rating: response.data.features[i].properties.rate,
-            name: response.data.features[i].properties.name,
-            categories: response.data.features[i].properties.kinds,
-            // city: data.data.results[0].components.city
-            cities: cities[i]
+            name: response.data.features[i].properties.name, 
+            categories: response.data.features[i].properties.kinds, 
+            city: data.data.results[0].components.city
           });
         });
 
-
-
-        console.log(placeObj)
         const places = await getCityInfo(placeObj)
-        // console.log('82',places);
+        console.log('82',places);
         res.render('destinations/destination-list', { places });
       })
         .catch(err => console.log(`Error finding the location's city: ${err}`));
