f4fa0119723f18cc7ed94202a66abc7c01a0d89e
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-09-05 11:04:53 -0400

    bootstrap style on signin and logout

--- bin/seed.js
+++ bin/seed.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const User = require('../models/user.model');
-// const Post = require('../models/post.model');
 const City = require('../models/city.model');
 
 const users = [
@@ -15,23 +14,6 @@ const users = [
     passwordHash: "$2a$10$ykwAIRV8uLcuwzUSmQX/e.XwWIAzOlzlp8pWrE7Zi/JZjNAQcfJ2S" // 1Password
   }
 ];
-
-// const posts = [
-//   {
-//     title: "Blog Test",
-//     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, pariatur aut. Similique, velit commodi. Aspernatur inventore debitis officia excepturi autem. Porro vel omnis doloribus consectetur in, dolorum sapiente id neque!",
-//     image: "https://res.cloudinary.com/dllcgl1lt/image/upload/v1598739087/iwmikpcenzvysm2qnsni.jpg",
-//     location: ["Seattle"],
-//     user: "5f4a7106f5c57a1e05ef9fbc" // user: tester1
-//   },
-//   {
-//     title: "Blog Test2",
-//     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, pariatur aut. Similique, velit commodi. Aspernatur inventore debitis officia excepturi autem. Porro vel omnis doloribus consectetur in, dolorum sapiente id neque!",
-//     image: "https://res.cloudinary.com/dllcgl1lt/image/upload/v1598819778/wpwwumyeovgqji2vqgdi.jpg",
-//     location: ["tatooine"],
-//     user: "5f4a7106f5c57a1e05ef9fbd" // user: tester2
-//   }
-// ];
 
 const cities = [
   {
@@ -13725,7 +13707,6 @@ require("../configs/db.config");
 
 // clear the users collection
 User.collection.drop();
-// Post.collection.drop();
 City.collection.drop();
 
 // seed the database with users and cities
@@ -13738,12 +13719,12 @@ User
           .then(citiesFromDB => {
             // prints the new users and cities for confirmation
             console.log({ newUsers });
-            // console.log({ citiesFromDB });
+            console.log(`citiesFromDB: ${citiesFromDB[0]}, number of cities add: ${citiesFromDB.length}`);
 
             // stops the database
             mongoose.disconnect();
           })
-          .catch(err => console.log(`Erro seeding Cities: ${err}`));
+          .catch(err => console.log(`Error seeding Cities: ${err}`));
 
   })
   .catch(err => console.log(`Error seeding Users: ${err}`));
